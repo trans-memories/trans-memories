f7b8dd577c9bca25743e70d485b86197850c8713
needhourger <841904608@qq.com>
2023-12-04 18:27:32 +0800

    feat: add star rotation

--- src/components/Starfield/index.js
+++ src/components/Starfield/index.js
@@ -7,41 +7,40 @@ const ALPHA_STEP = 0.02
 
 export default function StarField() {
   const createStars = () => {
-    var canvas = document.getElementById("starfield"),
+    const canvas = document.getElementById("starfield"),
       ctx = canvas.getContext("2d"),
-      w = (canvas.width = window.innerWidth),
-      h = (canvas.height = window.innerHeight),
+      WIDTH = (canvas.width = window.innerWidth),
+      HEIGHT = (canvas.height = window.innerHeight),
       hue = 220,
-      stars = [],
-      count = 0
-    var canvas2 = document.createElement("canvas"),
-      ctx2 = canvas2.getContext("2d");
-    canvas2.width = 100;
-    canvas2.height = 100;
-    var half = canvas2.width / 2,
-      gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-    gradient2.addColorStop(0.025, "#fff");
-    gradient2.addColorStop(0.1, "hsl(" + hue + ", 61%, 33%)");
-    gradient2.addColorStop(0.25, "hsl(" + hue + ", 64%, 6%)");
-    gradient2.addColorStop(1, "transparent");
-    ctx2.fillStyle = gradient2;
-    ctx2.beginPath();
-    ctx2.moveTo(50,0)
-    ctx2.quadraticCurveTo(40,60,100,50)
-    ctx2.quadraticCurveTo(40,40,50,100)
-    ctx2.quadraticCurveTo(60,40,0,50)
-    ctx2.quadraticCurveTo(60,60,50,0)
-    ctx2.fill();
+      stars = []
+    var count = 0
+    const starCanvas = document.createElement("canvas"),
+      starCtx = starCanvas.getContext("2d");
+    starCanvas.width = 100;
+    starCanvas.height = 100;
+    const half = starCanvas.width / 2,
+      starGradient = starCtx.createRadialGradient(half, half, 0, half, half, half);
+    starGradient.addColorStop(0.025, "#fff");
+    starGradient.addColorStop(0.1, "hsl(" + hue + ", 61%, 33%)");
+    starGradient.addColorStop(0.25, "hsl(" + hue + ", 64%, 6%)");
+    starGradient.addColorStop(1, "transparent");
+    starCtx.fillStyle = starGradient;
+    starCtx.beginPath();
+    starCtx.moveTo(50,0)
+    starCtx.quadraticCurveTo(40,60,100,50)
+    starCtx.quadraticCurveTo(40,40,50,100)
+    starCtx.quadraticCurveTo(60,40,0,50)
+    starCtx.quadraticCurveTo(60,60,50,0)
+    starCtx.fill();
     // End cache
 
     var Star = function () {
       this.radius = random(5, 50);
-      this.orbitX = w / 2;
-      this.orbitY = h / 2;
+      this.angle = random(0,90)
       this.timePassed = random(0, MAX_STARS);
       this.alpha = random(2, 10) / 10;
-      this.x = random(w)
-      this.y = random(h)
+      this.x = random(WIDTH)
+      this.y = random(HEIGHT)
       count++;
       stars[count] = this;
     };
@@ -52,14 +51,18 @@ export default function StarField() {
       } else if (twinkle === 2 && this.alpha < 1) {
         this.alpha += ALPHA_STEP;
       }
+      ctx.save()
+      ctx.translate(this.x, this.y)
+      ctx.rotate(this.angle * Math.PI / 180)
       ctx.globalAlpha = this.alpha;
       ctx.drawImage(
-        canvas2,
-        this.x - this.radius / 2,
-        this.y - this.radius / 2,
+        starCanvas,
+        - this.radius / 2,
+        - this.radius / 2,
         this.radius,
         this.radius
       );
+      ctx.restore()
       this.timePassed += this.speed;
     };
     for (var i = 0; i < MAX_STARS; i++) {
@@ -69,7 +72,7 @@ export default function StarField() {
       ctx.globalCompositeOperation = "source-over";
       ctx.globalAlpha = 0.5;
       ctx.fillStyle = "hsla(" + hue + ", 16%, 4%, 1)";
-      ctx.fillRect(0, 0, w, h);
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
       ctx.globalCompositeOperation = "lighter";
       for (var i = 1, l = stars.length; i < l; i++) {
         stars[i].draw();
